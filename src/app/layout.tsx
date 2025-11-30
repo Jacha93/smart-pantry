ec659e638f108fc0ab073ffccfbe0da74264abc9
Jacha93 <lj.show.93@gmail.com>
2025-11-30 02:00:01 +0100

    fix: robust crypto.randomUUID polyfill for non-secure contexts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,11 +31,11 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              if (typeof crypto === 'undefined') {
+              if (typeof window.crypto === 'undefined') {
                 window.crypto = {};
               }
-              if (typeof crypto.randomUUID === 'undefined') {
-                crypto.randomUUID = function() {
+              if (typeof window.crypto.randomUUID === 'undefined') {
+                window.crypto.randomUUID = function() {
                   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                     return v.toString(16);
